b9c5997aa91a8c6799dec61621107c16f250e6b8
life4coding <life4coding@gmail.com>
2016-03-11 17:16:57 +0800

    finish part for animition Cross dissolve

---
+++ CustomTransitionDemoA/ViewControllerA.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  CustomTransitionDemoA
+//
+//  Created by kiwi on 16/3/11.
+//  Copyright © 2016年 life4coding. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerA: UIViewController , UIViewControllerTransitioningDelegate{
+
+    
+   lazy var secondViewController: ViewControllerB = ViewControllerB()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        secondViewController = self.storyboard?.instantiateViewControllerWithIdentifier("ViewControllerB") as! ViewControllerB
+        
+        
+        secondViewController.modalPresentationStyle = .FullScreen
+        secondViewController.transitioningDelegate = self
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func presentButtonPressed(sender: AnyObject) {
+        
+       
+        
+        self.presentViewController(secondViewController, animated: true , completion: nil)
+        
+    }
+    
+    
+    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+      
+        return CustomAnimator()
+        
+    }
+    
+    
+    func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return CustomAnimator()
+    }
+
+    
+}
+
---
+++ CustomTransitionDemoA/ViewControllerB.swift
@@ -0,0 +1,35 @@
+//
+//  ViewControllerB.swift
+//  CustomTransitionDemoA
+//
+//  Created by kiwi on 16/3/11.
+//  Copyright © 2016年 life4coding. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerB: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
