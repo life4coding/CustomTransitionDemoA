1815caf04b3e19dbac939f3b5413a6633f1b5b1b
life4coding <life4coding@gmail.com>
2016-03-11 10:58:53 +0800

    Initial Commit

---
+++ CustomTransitionDemoA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomTransitionDemoA
+//
+//  Created by kiwi on 16/3/11.
+//  Copyright © 2016年 life4coding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
